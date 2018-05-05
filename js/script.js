bfae513c435a14e5a607e2927e1587263af0476d
Sylwia Krzymowska <s.krzymowska@gmail.com>
2018-05-05 10:06:04 +0200

    Create new repository

---
+++ js/script.js
@@ -0,0 +1,13 @@
+function checkOnclickEvent() {
+  console.log();
+}
+
+var list = document.getElementById('list'),
+var add = document.getElementById('addElem');
+
+add.addEventListener('click', function() {
+    var element = document.createElement('li'),
+        itemsByTagName = list.getElementsByTagName('li'),
+    element.innerHTML = ('item' + itemsByTagName.lenght,
+    list.appendChild(element);
+});
